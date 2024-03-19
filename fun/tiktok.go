9ef5c1799f4db32f2f38f490f97db4f064b2f3df
Supa <supa.codes@gmail.com>
2024-03-19 07:29:51 +0200

    + use upload func

--- fun/tiktok.go
+++ fun/tiktok.go
@@ -1,13 +1,8 @@
 package fun
 
 import (
-	"bytes"
-	"encoding/json"
 	"errors"
-	"io"
-	"mime/multipart"
 	"net/http"
-	"net/textproto"
 	"os/exec"
 	"strings"
 
@@ -15,18 +10,7 @@ import (
 	regexp "github.com/wasilibs/go-re2"
 )
 
-type Upload struct {
-	ID       string `json:"id"`
-	Ext      string `json:"ext"`
-	Type     string `json:"type"`
-	Checksum string `json:"checksum"`
-	Key      string `json:"key"`
-	Link     string `json:"link"`
-	Delete   string `json:"delete"`
-}
-
 func init() {
-	client := http.Client{}
 	links := regexp.MustCompile(`(?i)\S*tiktok\.com\/\S+|\S*instagram\.com\/(reels?|p)\/\S+`)
 
 	F.Register(&Cmd{
@@ -47,7 +31,7 @@ func init() {
 				return err
 			}
 
-			res, err := client.Get(strings.TrimSuffix(string(out), "\n"))
+			res, err := apiClient.Get(strings.TrimSuffix(string(out), "\n"))
 			if err != nil {
 				return err
 			}
@@ -65,33 +49,9 @@ func init() {
 				goto retry
 			}
 
-			fileBuf := &bytes.Buffer{}
-			writer := multipart.NewWriter(fileBuf)
-
-			h := make(textproto.MIMEHeader)
-			h.Set("Content-Disposition", `form-data; name="file"; filename="res.mp4"`)
-			h.Set("Content-Type", res.Header.Get("Content-Type"))
-
-			part, err := writer.CreatePart(h)
-			if _, err := io.Copy(part, res.Body); err != nil {
-				return err
-			}
-			writer.Close()
-
-			res, err = client.Post("https://kappa.lol/api/upload?skip-cd=true", writer.FormDataContentType(), fileBuf)
+			upload, err := UploadFile(res.Body, "res.mp4", res.Header.Get("Content-Type"))
 			if err != nil {
-				return err
-			}
-			defer res.Body.Close()
-
-			buf, err := io.ReadAll(res.Body)
-			var upload Upload
-			if err := json.Unmarshal(buf, &upload); err != nil {
-				return err
-			}
-
-			if res.StatusCode != http.StatusOK {
-				return errors.New(string(buf))
+				return
 			}
 
 			_, err = Say(m.RoomID, "mirror: "+upload.Link+upload.Ext, m.ID)
