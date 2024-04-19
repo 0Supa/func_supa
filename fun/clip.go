f120d79d6e82a821a495a04548a7af0da27308d6
0Supa <supa.codes@gmail.com>
2024-04-19 21:08:00 +0300

    update url

--- fun/clip.go
+++ fun/clip.go
@@ -52,7 +52,7 @@ func init() {
 				return
 			}
 
-			_, err = Say(m.RoomID, "https://clips.supa.sh/"+url.PathEscape(c.File), m.ID)
+			_, err = Say(m.RoomID, "https://fi.supa.sh/clips/"+url.PathEscape(c.File), m.ID)
 			return
 		},
 	})
