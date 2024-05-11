4be8641357ec78865904f216c63a6bbd5b0c9a15
0Supa <supa.codes@gmail.com>
2024-05-11 15:11:42 +0300

    Update tiktok.go

--- fun/tiktok.go
+++ fun/tiktok.go
@@ -38,7 +38,8 @@ func init() {
 			out, err := cmd.Output()
 			if err != nil {
 				if exit, ok := err.(*exec.ExitError); ok && exit.ExitCode() == 1 {
-					return fmt.Errorf("%s:\n%s", err.Error(), exit.Stderr)
+					fmt.Printf("%s:\n%s\n", err.Error(), exit.Stderr)
+					return nil
 				}
 				return err
 			}
