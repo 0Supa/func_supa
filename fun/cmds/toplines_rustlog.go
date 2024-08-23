d00f7a81eb05d026eeb9356dd1e7127ed76b8df3
Supa <36031171+0Supa@users.noreply.github.com>
2024-08-24 02:26:45 +0300

    + toplines cmd

---
+++ fun/cmds/toplines_rustlog.go
@@ -0,0 +1,68 @@
+package fun
+
+import (
+	"context"
+	"io"
+	"strconv"
+	"strings"
+
+	. "github.com/0supa/func_supa/fun"
+	logs_db "github.com/0supa/func_supa/fun/api/clickhouse_db"
+	api_kappa "github.com/0supa/func_supa/fun/api/kappa"
+	. "github.com/0supa/func_supa/fun/api/twitch"
+	"github.com/0supa/func_supa/fun/utils"
+	"github.com/gempir/go-twitch-irc/v4"
+	"github.com/olekukonko/tablewriter"
+)
+
+func init() {
+	Fun.Register(&Cmd{
+		Name: "toplines",
+		Handler: func(m twitch.PrivateMessage) (err error) {
+			args := strings.Split(m.Message, " ")
+			if !utils.IsPrivileged(m.User.ID) || len(args) < 2 || args[0] != "`toplines" {
+				return
+			}
+
+			user, err := GetUser(args[1], "")
+			if err != nil {
+				Say(m.RoomID, "failed getting user: "+err.Error(), m.ID)
+				return
+			}
+
+			if user.ID == "" {
+				Say(m.RoomID, "user not found", m.ID)
+				return
+			}
+
+			rows, err := logs_db.Clickhouse.Query(context.Background(), "SELECT channel_login, count() AS lines FROM rustlog_zonian.message_structured WHERE user_id=? GROUP BY channel_login ORDER BY lines DESC", user.ID)
+			if err != nil {
+				return
+			}
+			defer rows.Close()
+
+			tableString := &strings.Builder{}
+			tableString.WriteString("Top lines by @" + user.Login + "\n\n")
+			table := tablewriter.NewWriter(tableString)
+			table.SetHeader([]string{"Channel", "Lines"})
+
+			var cLogin string
+			var lineCount int
+			for rows.Next() {
+				if err := rows.Scan(&cLogin, &lineCount); err != nil {
+					return err
+				}
+				table.Append([]string{cLogin, strconv.Itoa(lineCount)})
+			}
+
+			rc := io.NopCloser(strings.NewReader(tableString.String()))
+			defer rc.Close()
+
+			upload, err := api_kappa.UploadFile(rc, "dat.txt", "text/plain")
+
+			_, err = Say(m.RoomID, upload.Link, m.ID)
+
+			return err
+		},
+	})
+}
--- go.mod
+++ go.mod
@@ -16,6 +16,8 @@ require (
 	github.com/go-faster/errors v0.7.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.17.7 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/paulmach/orb v0.11.1 // indirect
 	github.com/pierrec/lz4/v4 v4.1.21 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
